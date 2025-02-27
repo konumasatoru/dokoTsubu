5126b3180aeb814e72f4a0868be20616ffae985d
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-02-28 03:59:44 +0900

    つぶやきに関するリクエストを処理するコントローラー（修正）

--- dokoTsubu/src/main/java/servlet/Main.java
+++ dokoTsubu/src/main/java/servlet/Main.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 @WebServlet("/Main")
@@ -44,4 +45,118 @@ public class Main extends HttpServlet {
 			dispatcher.forward(request, response);
 		}
 	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		//入力値チェック
+		if (text != null && text.length() != 0) {
+			//アプリケーションスコープに保存されたつぶやきリストを取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>)application.getAttribute("loginUser");
+			
+			//セッションスコープに保存されたユーザー情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User)session.getAttribute("loginUser");
+			
+			//つぶやきを作成してつぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			//アプリケーションスコープにつぶやきリストを保存
+			application.setAttribute("mutterList", mutterList);
+		}
+		
+		//メイン画面にフォワード
+		RequestDispatcher dispatcher =request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request,  response);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
